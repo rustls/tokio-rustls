ad26b8342811b89e30ff15e83ef7004c658ab8f2
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-08-04 11:11:26 +0200

    Update webpki-roots dev-dependency

--- examples/client.rs
+++ examples/client.rs
@@ -56,7 +56,7 @@ async fn main() -> io::Result<()> {
         });
         root_cert_store.add_server_trust_anchors(trust_anchors);
     } else {
-        root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
+        root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(
             |ta| {
                 OwnedTrustAnchor::from_subject_spki_name_constraints(
                     ta.subject,
