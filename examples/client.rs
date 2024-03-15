c437be5a2751b2e45ce11b84e3764690d8c32654
Matthew <matthew@spaceshipaudio.co.uk>
2024-03-15 13:45:46 +0000

    Make sure imports in examples use re-exported rustls types

--- examples/client.rs
+++ examples/client.rs
@@ -8,6 +8,7 @@ use std::sync::Arc;
 use argh::FromArgs;
 use tokio::io::{copy, split, stdin as tokio_stdin, stdout as tokio_stdout, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio_rustls::rustls;
 use tokio_rustls::TlsConnector;
 
 /// Tokio Rustls client example
