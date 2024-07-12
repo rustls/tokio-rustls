b7e45fa0ce8d4ec488e7b10b2578fe8b02b1a7d8
Daniel McCarney <daniel@binaryparadox.net>
2024-07-12 09:25:22 -0400

    tests: rework vendored certificates/keys
    The existing unit tests used vendored cert/key data in a strange way.
    The `end.cert` and `end.chain` files were the same, and neither was
    a chain. In both cases the certificate was self-signed, and that same
    certificate was also configured as a trust anchor in the client
    configurations. No code/script was included to regenerate the cert (and
    it was set to expire in Aug).
    
    This commit replaces the test files to better simulate a real-world
    deployment with a trust anchor configured OOB and an intermediate and
    end-entity chain served by the TLS server.
    
    The test certificates are switched to use ECDSA (the rcgen default) for
    private keys instead of RSA. RSA is for the 90s and ECDSA will be faster
    :)
    
    No tests presently require the root or intermediate private keys, or
    a serialization of just the end entity cert without the intermediate, so
    we don't persist this data. This could be added in the future as req'd.
    
    All of the key/cert generation is bundled into an ignored integration
    test `tests/certs/main.rs` using a new dev-only dep on `rcgen`. This
    felt like the best option on balance, but we could also create a second
    crate, or look at the unstable nightly Cargo script feature.

--- tests/utils.rs
+++ tests/utils.rs
@@ -7,14 +7,19 @@ mod utils {
 
     #[allow(dead_code)]
     pub fn make_configs() -> (ServerConfig, ClientConfig) {
-        const CERT: &str = include_str!("certs/end.cert");
-        const CHAIN: &str = include_str!("certs/end.chain");
-        const RSA: &str = include_str!("certs/end.rsa");
+        // A test root certificate that is the trust anchor for the CHAIN.
+        const ROOT: &str = include_str!("certs/root.pem");
+        // A server certificate chain that includes both an end-entity server certificate
+        // and the intermediate certificate that issued it. The ROOT is configured
+        // out-of-band.
+        const CHAIN: &str = include_str!("certs/chain.pem");
+        // A private key corresponding to the end-entity server certificate in CHAIN.
+        const EE_KEY: &str = include_str!("certs/end.key");
 
-        let cert = certs(&mut BufReader::new(Cursor::new(CERT)))
+        let cert = certs(&mut BufReader::new(Cursor::new(CHAIN)))
             .map(|result| result.unwrap())
             .collect();
-        let key = private_key(&mut BufReader::new(Cursor::new(RSA)))
+        let key = private_key(&mut BufReader::new(Cursor::new(EE_KEY)))
             .unwrap()
             .unwrap();
         let sconfig = ServerConfig::builder()
@@ -23,9 +28,9 @@ mod utils {
             .unwrap();
 
         let mut client_root_cert_store = RootCertStore::empty();
-        let mut chain = BufReader::new(Cursor::new(CHAIN));
-        for cert in certs(&mut chain) {
-            client_root_cert_store.add(cert.unwrap()).unwrap();
+        let mut roots = BufReader::new(Cursor::new(ROOT));
+        for root in certs(&mut roots) {
+            client_root_cert_store.add(root.unwrap()).unwrap();
         }
 
         let cconfig = ClientConfig::builder()
